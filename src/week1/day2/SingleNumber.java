1f56648e17b2be06d49af4a9560ee602abe4dd86
Sandra Binoy <sandra.binoy@outlook.com>
2021-06-29 20:39:45 +0530

    SingleNumber added

---
+++ src/week1/day2/SingleNumber.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+
+        int length = nums.length;
+        int number = -1;
+
+        if(length == 1) {
+            return nums[0];
+        }
+
+        for(int i = 0; i < length; i++) {
+
+        }
+
+        return number;
+
+    }
+
+}
