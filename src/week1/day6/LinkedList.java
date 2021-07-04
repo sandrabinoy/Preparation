2832a06773341aacea822349ea4d4c9d9f6d66ec
Sandra Binoy <sandra.binoy@outlook.com>
2021-07-04 21:17:02 +0530

    LinkedList added

---
+++ src/week1/day6/LinkedList.java
@@ -0,0 +1,13 @@
+package week1.day6;
+
+public class LinkedList<Item> {
+
+    private Node<Item> linkedList;
+
+    public void add(int data) {
+
+
+
+    }
+
+}
---
+++ src/week1/day6/Node.java
@@ -0,0 +1,14 @@
+package week1.day6;
+
+public class Node<Item> {
+
+    private Item data;
+    private Node<Item> next;
+
+    public Node (Item data) {
+
+        this.data = data;
+
+    }
+
+}
